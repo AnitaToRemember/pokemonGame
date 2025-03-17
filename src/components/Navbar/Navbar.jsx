ae95db14c83ad7c00b5a5c7aea6cc8d30e35c774
AnitaToRemember <best.anac@gmail.com>
2025-03-17 19:13:42 +0100

    Nav bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,44 +5,55 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 import "../Navbar/Navbar.css";
 
 function Navbar() {
-const [active, setActive] = useState(false);
+    const [active, setActive] = useState(false);
 
-const links = [
-    { page: "Home", href: "/" },
-    { page: "Portfolio", href: "https://bbds-group.com/" },
-    { page: "Services", href: "https://bbds-group.com/our-services/" },
-    { page: "LinkedIn", href: "https://www.linkedin.com/in/anabest/" },
-];
+    const links = [
+    { page: "Home", href: "/", external: false },
+    { page: "Portfolio", href: "https://bbds-group.com/", external: true },
+    { page: "Services", href: "https://bbds-group.com/our-services/", external: true },
+    { page: "LinkedIn", href: "https://www.linkedin.com/in/anabest/", external: true },
+    ];
 
-const toggleNavbar = () => {
+    const toggleNavbar = () => {
     setActive(!active);
-};
+    };
 
-const handleNavLinkClick = () => {
+    const handleNavLinkClick = () => {
     setActive(false);
-};
+    };
 
 return (
     <nav className={`navbar ${active ? "open" : ""}`}>
-    <div className="logo">Guess the Pokémon Game</div>
+        <div className="logo">Guess the Pokémon Game </div>
 
-    <FontAwesomeIcon
+        <FontAwesomeIcon
         className="menu-icon"
         icon={faBars}
         onClick={toggleNavbar}
-    />
+        />
 
-    <ul className={`nav-links ${active ? "active" : ""}`}>
-        {links.map(({ page, href }) => (
-        <li key={page}>
-            <NavLink to={href} onClick={handleNavLinkClick}>
-            {page}
-            </NavLink>
-        </li>
-        ))}
-    </ul>
+        <ul className={`nav-links ${active ? "active" : ""}`}>
+        {links.map(({ page, href, external }) => (
+                <li key={page}>
+                    {external ? (
+                        <a
+                        href={href}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        onClick={handleNavLinkClick}
+                        >
+                        {page}
+                        </a>
+                    ) : (
+                        <NavLink to={href} onClick={handleNavLinkClick}>
+                        {page}
+                        </NavLink>
+                    )}
+                </li>
+            ))}
+        </ul>
     </nav>
-);
+  );
 }
 
 export default Navbar;
